82ccc7a968761ffb6d53d37b070f671f4a0418d4
lightOFdarkness830 <71855399+lightOFdarkness830@users.noreply.github.com>
2020-10-04 17:51:28 +0530

    Create q2pt1.java

---
+++ java/assignment8/q2pt1.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question2pt1
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array that is to be created: ");
+		int size = sc.nextInt();
+		int[] myArray = new int[size];
+		System.out.println("Enter the elements of the array: ");
+		for(int i=0; i<size; i++)
+		{
+			myArray[i] = sc.nextInt();
+		}
+		System.out.println("The Array created is: "+Arrays.toString(myArray));
+		System.out.println("Resultant array: ");
+		int pos = 0;
+		for(int i=0; i<myArray.length; i++)
+		{
+			if(myArray[i]!=0)
+			{
+				myArray[pos]=myArray[i];
+				pos++;
+			}
+		}
+		while(pos<myArray.length)
+		{
+			myArray[pos] = 0;
+			pos++;
+		}
+		System.out.println("The Array created is: "+Arrays.toString(myArray));
+	}
+}
